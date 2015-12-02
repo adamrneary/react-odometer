4f43220c2962fe0120e4ca33a1e81a1f0e80a2b0
liranhershko <liran.hershko@gmail.com>
2015-12-02 21:38:10 +0200

    react 0.14 compatibility fix

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var React = require('react');
+var ReactDom = require('react-dom');
 
 var OdometerComponent = React.createClass({
-  componentDidMount: function(){
-     this.odometer = new Odometer({
-      el: this.getDOMNode(),
-      value: this.props.value
-    });
-  },
-  componentDidUpdate: function() {
-   this.odometer.update(this.props.value)
-  },
-  render: function() {
-    return React.DOM.div()
-  }
+    componentDidMount: function(){
+        this.odometer = new Odometer({
+            el: ReactDom.findDOMNode(this),
+            value: this.props.value
+        });
+    },
+    componentDidUpdate: function() {
+        this.odometer.update(this.props.value)
+    },
+    render: function() {
+        return React.DOM.div()
+    }
 })
 
 module.exports = OdometerComponent;
