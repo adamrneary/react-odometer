119dc1be95ad07a180e19a2d00edd15c433d02e7
Derek Keith <derek@codeurge.com>
2016-10-06 05:29:23 -0400

    update for react 0.15.x

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+var Odometer = require('odometer');
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 var OdometerComponent = React.createClass({
   componentDidMount: function(){
-     this.odometer = new Odometer({
-      el: this.getDOMNode(),
-      value: this.props.value
+    this.odometer = new Odometer({
+      el: ReactDOM.findDOMNode(this),
+      duration: this.props.duration || 2000,
+      value: 1000,
+      format: this.props.format || '',
+      theme: this.props.theme || 'default',
     });
   },
-  componentDidUpdate: function() {
-   this.odometer.update(this.props.value)
+  componentWillUpdate: function(nextProps) {
+    this.odometer.update(`${1000 + nextProps.value}`);
   },
   render: function() {
     return React.DOM.div()
   }
-})
+});
+
+OdometerComponent.propTypes = {
+  value: React.PropTypes.number.isRequired,
+};
 
 module.exports = OdometerComponent;
